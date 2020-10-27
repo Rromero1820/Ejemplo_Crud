2acdd4e8ab0f77666dbc5624ec30ce32dd6bd582
Rromero1820 <63007748+Rromero1820@users.noreply.github.com>
2020-10-27 06:59:40 -0500

    Ejemplo de actualizacion en db.php

--- php_crud_mysql/db.php
+++ php_crud_mysql/db.php
@@ -4,7 +4,7 @@ session_start();
 
 $conn = mysqli_connect('localhost', 'root', '', 'php_mysql_crud');
 
-// if (isset($conn)) {
+// if (isset($conn)) {  =solo para ver si aparece=
 //   echo "Estamos ya conectados tio";
 // }
 
